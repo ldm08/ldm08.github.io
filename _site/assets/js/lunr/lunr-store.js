756acc1eb80cca72cd1fd6412b581df8f09a5010
ldm08 <ldugom@icloud.com>
2020-07-07 23:52:14 -0400

    revert to last working state

--- _site/assets/js/lunr/lunr-store.js
+++
@@ -1,7 +0,0 @@
-var store = [{
-        "title": "Airbnb Washington D.C. EDA",
-        "excerpt":" ","categories": [],
-        "tags": [],
-        "url": "http://localhost:4000/airbnb-eda-washingtondc/",
-        "teaser": null
-      }]
