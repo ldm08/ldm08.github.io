fcbd56dd3e82fed00ead569850f28dae61d1a39b
ldm08 <ldugom@icloud.com>
2020-07-08 22:55:14 -0400

    merge

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Airbnb Washington D.C. EDA",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/airbnb-eda-washingtondc/",
+        "teaser": "http://localhost:4000/images/la%20jolla.png"
+      }]
